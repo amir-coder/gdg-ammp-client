4793a4e0f57471d32f46acca43385efaf32ccd95
Nwasler <107283332+Nwasler@users.noreply.github.com>
2023-02-07 14:03:04 +0100

    Create Events.js

---
+++ src/pages/Events.js
@@ -0,0 +1,7 @@
+export default function Events() {
+    return (
+        <div className='flex h-full flex-col justify-center items-center'>
+            <h3 className='text-4xl mb-5 font-bold'>Events</h3>
+        </div>
+    );
+}
