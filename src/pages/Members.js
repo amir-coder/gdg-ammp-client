449f851065897fab8c598b990e6f969a281aaa51
Nwasler <107283332+Nwasler@users.noreply.github.com>
2023-02-07 14:04:44 +0100

    Update Members.js

--- src/pages/Members.js
+++ src/pages/Members.js
@@ -1,23 +1,7 @@
-import { useState } from "react";
-import Table from "./table";
-
-
-const Members = () => {
-    const [data, setData] = useState([
-        { Members: 'Ali', IDK: 1234214, JoiningDate: 'May 19,2019', Department: 'dev' },
-        { Members: 'cheima', IDK: 1234214, JoiningDate: 'May 19,2019', Department: 'design' },
-        { Members: 'amani', IDK: 1234214, JoiningDate: 'May 19,2019', Department: 'logistics' },
-        { Members: 'moh', IDK: 1234214, JoiningDate: 'May 19,2019', Department: 'dev' },
-        { Members: 'meroua', IDK: 1234214, JoiningDate: 'May 19,2019', Department: 'ui/ux' },
-        { Members: 'mokran', IDK: 1234214, JoiningDate: 'May 19,2019', Department: 'ui/ux' },
-        { Members: 'massi', IDK: 1234214, JoiningDate: 'May 19,2019', Department: 'dev' },
-        { Members: 'Ali', IDK: 1234214, JoiningDate: 'May 19,2019', Department: 'logistics' }
-
-    ])
+export default function Members() {
     return (
-
-        <Table data={data} />
+        <div className='flex h-full flex-col justify-center items-center'>
+            <h3 className='text-4xl mb-5 font-bold'>Members</h3>
+        </div>
     );
 }
-
-export default Members;
