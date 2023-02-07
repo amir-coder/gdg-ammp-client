db204697fcb157c6728f40aae33a7b7b1d00b7f5
Nwasler <107283332+Nwasler@users.noreply.github.com>
2023-02-07 14:03:23 +0100

    Create Projects.js

---
+++ src/pages/Projects.js
@@ -0,0 +1,9 @@
+//Projects
+
+export default function Projects() {
+    return (
+        <div className='flex h-full flex-col justify-center items-center'>
+            <h3 className='text-4xl mb-5 font-bold'>Projects</h3>
+        </div>
+    );
+}
