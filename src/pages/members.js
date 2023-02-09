4cada7fa0e1e07400ed9d15eda99f56494c946ee
amir-coder <ja_amir@esi.dz>
2023-02-09 20:33:13 +0100

    integrated events and memebrs

--- src/pages/members.js
+++ src/pages/members.js
@@ -1,28 +1,7 @@
-import { useState } from "react";
-import Table from "./comps/table";
-import Layout from '../../components/layout';
-
-const Members = () => {
-    const [data, setData] = useState([
-        {Members: "meriem", IDK:"29374",JoiningDate:"May9,2019",Department:"dev"},
-        {Members: "ali", IDK:"379247",JoiningDate:"May10,2019",Department:"ui/ux"},
-        {Members: "bilal", IDK:"29374",JoiningDate:"May9,2019",Department:"dev"},
-        {Members: "mokrane", IDK:"2342134",JoiningDate:"May9,2020",Department:"dev"},
-        {Members: "cheima", IDK:"29374",JoiningDate:"May9,2018",Department:"events"},
-        {Members: "amani", IDK:"29374",JoiningDate:"May9,2019",Department:"dev"},
-        {Members: "ali", IDK:"243522",JoiningDate:"March9,2019",Department:"dev"},
-        {Members: "ali", IDK:"29374",JoiningDate:"May19,2019",Department:"ui/ux"}
-
-
-    ])
+export default function Members() {
     return (
-        <Table data ={data} />
+        <div className='flex h-full flex-col justify-center items-center'>
+            <h3 className='text-4xl mb-5 font-bold'>Members</h3>
+        </div>
     );
 }
- 
-Members.getLayout = function getLayout(page) {
-    return (
-        <Layout>{page}</Layout>
-    )
-  }
-export default Members;
